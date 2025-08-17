9a57841efbfca48716b0dabf844f604686028777
Sneha-Gupta951 <snehagupta0203200@gmail.com>
2025-08-17 10:35:09 +0530

    complete both task

---
+++ task1_task2.py
@@ -0,0 +1,23 @@
+# task1
+
+a = input("Enter a number1: ")
+b = input("Enter a number2: ")
+
+addition = a + b
+print("addition: ", addition)
+
+subtraction = a - b
+print("subtraction: ", subtraction)
+
+multiplication = a * b
+print("multiplication: ", multiplication)
+
+division = a / b
+print("division: ", division)
+
+# task2
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = first_name + " " + last_name
+print("hello " + full_name + "! welcome to python world")
